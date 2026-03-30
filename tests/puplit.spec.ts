f4a0bdca97a5c4495e0faeb7f812c3ab3af1c88d
Filip <Filip.Kwiatek@gmail.com>
2026-03-30 22:32:45 +0200

    run script added

--- tests/puplit.spec.ts
+++ tests/puplit.spec.ts
@@ -5,7 +5,7 @@ test.describe("Puplit test", () => {
     const userId = "tester12";
     const userPassword = "12312312";
 
-    await page.goto('/');
+    await page.goto("/");
 
     await page.getByTestId("login-input").fill(userId);
     await page.getByTestId("password-input").fill(userPassword);
@@ -49,6 +49,29 @@ test.describe("Puplit test", () => {
     // Assert
     // await expect(page.getByRole('link', { name: 'Doładowanie wykonane! 40,00PLN na numer 500 xxx xxx' })).toBeVisible();
     await expect(page.locator("#show_messages")).toHaveText(expectedMessage);
+    await page.getByTestId("close-button").click();
+  });
+
+  test("correct balance after successful mobile top-up with correct data", async ({
+    page,
+  }) => {
+    // Arrange
+    const expectedTopUpReceiver = "500 xxx xxx";
+    const topUpAmount = "40";
+    const expectedMessage = `Doładowanie wykonane! ${topUpAmount},00PLN na numer ${expectedTopUpReceiver}`;
+    const initialBalance = await page.locator("#money_value").innerText();
+    const expectedBalance = Number(initialBalance) - Number(topUpAmount);
+    // Act
+    await page
+      .locator("#widget_1_topup_receiver")
+      .selectOption(expectedTopUpReceiver);
+    await page.locator("#widget_1_topup_amount").fill(topUpAmount);
+    await page.getByText("zapoznałem się z regulaminem").click();
+    await page.getByRole("button", { name: "doładuj telefon" }).click();
+
+    // Assert
+    // await expect(page.getByRole('link', { name: 'Doładowanie wykonane! 40,00PLN na numer 500 xxx xxx' })).toBeVisible();
+    await expect(page.locator("#money_value")).toHaveText(`${expectedBalance}`);
     await page.getByTestId("close-button").click();
   });
 });
