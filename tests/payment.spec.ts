b9bf146b7c3a7f140bbe2316d16a75f089a4b6b5
Filip <Filip.Kwiatek@gmail.com>
2026-04-08 13:34:54 +0200

    Refactor login handling by creating a LoginPage class for better code organization and reuse in login.spec.ts, payment.spec.ts, and puplit.spec.ts

--- tests/payment.spec.ts
+++ tests/payment.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { loginData } from "../test-data/login.data";
+import { LoginPage } from "../pages/login.page";
 
 test.describe("Payment test", () => {
   test.beforeEach(async ({ page }) => {
@@ -8,9 +9,10 @@ test.describe("Payment test", () => {
 
     await page.goto("/");
 
-    await page.getByTestId("login-input").fill(userId);
-    await page.getByTestId("password-input").fill(userPassword);
-    await page.getByTestId("login-button").click();
+    const loginPage = new LoginPage(page);
+    await loginPage.loginInput.fill(userId);
+    await loginPage.passwordInput.fill(userPassword);
+    await loginPage.loginButton.click();
     await page.getByRole("link", { name: "płatności" }).click();
   });
   test("simple payment", async ({ page }) => {
