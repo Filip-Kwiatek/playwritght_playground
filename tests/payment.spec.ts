62aaeddd08b5578035566bc5ecba04e6ce1d2663
Filip <Filip.Kwiatek@gmail.com>
2026-04-13 12:19:16 +0200

    Added POM to payments tests

--- tests/payment.spec.ts
+++ tests/payment.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { loginData } from "../test-data/login.data";
 import { LoginPage } from "../pages/login.page";
+import { PaymentPage } from "../pages/payment.page";
 
 test.describe("Payment test", () => {
   test.beforeEach(async ({ page }) => {
@@ -15,18 +16,24 @@ test.describe("Payment test", () => {
     await loginPage.loginButton.click();
     await page.getByRole("link", { name: "płatności" }).click();
   });
-  test("simple payment", async ({ page }) => {
+  test.only("simple payment", async ({ page }) => {
     // Arrange
     const exportReceiver = "Jan Nowak";
     const transferAccount = "12 3456 7890 1234 5678 9012 34568";
     const transferAmount = "222";
     const expectedMessage = `Przelew wykonany! ${transferAmount},00PLN dla ${exportReceiver}`;
     // Act
-    await page.getByTestId("transfer_receiver").fill(exportReceiver);
-    await page.getByTestId("form_account_to").fill(transferAccount);
-    await page.getByTestId("form_amount").fill(transferAmount);
-    await page.getByRole("button", { name: "wykonaj przelew" }).click();
-    await page.getByTestId("close-button").click();
+    const paymentPage = new PaymentPage(page);
+    await paymentPage.transferReceiver.fill(exportReceiver);
+    await paymentPage.transferAccount.fill(transferAccount);
+    await paymentPage.transferAmount.fill(transferAmount);
+    await paymentPage.paymentButton.click();
+    await paymentPage.paymentCloseButton.click();
+    // await page.getByTestId("transfer_receiver").fill(exportReceiver);
+    // await page.getByTestId("form_account_to").fill(transferAccount);
+    // await page.getByTestId("form_amount").fill(transferAmount);
+    // await page.getByRole("button", { name: "wykonaj przelew" }).click();
+    // await page.getByTestId("close-button").click();
 
     // Assert
     await expect(page.locator("#show_messages")).toHaveText(expectedMessage);
